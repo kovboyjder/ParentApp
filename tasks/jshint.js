9390d9e977da0fae6acbb9c49674747d89a99fda
kovboyjder <jakob.m.kjaer@gmail.com>
2017-12-07 12:31:38 +0000

    delete tasks

--- tasks/jshint.js
+++
@@ -1,9 +0,0 @@
-module.exports = function(grunt) {
-  grunt.config('jshint', {
-    app: {
-      options: {jshintrc: 'app/.jshintrc'},
-      src: ['app/**/*.js'],
-    },
-  });
-  grunt.loadNpmTasks('grunt-contrib-jshint');
-};
