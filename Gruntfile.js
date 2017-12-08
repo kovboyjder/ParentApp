0a22bb7043612c36c4390da49c46d9189a037336
Jakob Marius Kjaer <jakob.kjaer@itelligence.dk>
2017-12-08 11:26:26 +1000

    Update Gruntfile.js

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,18 +1,19 @@
 module.exports = function(grunt) {
 
-// Initialize config.
   grunt.initConfig({
-    pkg: require("./package.json")
+        pkg: require("./package.json")
   });
 
   // Load per-task config from separate files.
   grunt.loadTasks("tasks");
+ 
 
-  grunt.registerTask("default", [
+  grunt.registerTask('default', [
     "lint",
     "clean",
     "build",
-    "jsdoc"
-   ]);
+    "nwabap_ui5uploader"
+
+  ]);
 
 };
